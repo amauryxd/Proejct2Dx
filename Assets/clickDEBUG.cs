14763b74e45ca5ae3578991e29cf84b9aaeaee6e
amauryxd <cdmx2472@amerike.edu.mx>
2024-08-28 16:07:21 -0600

    yatrabajemucho

---
+++ Assets/clickDEBUG.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickDEBUG : MonoBehaviour
+{
+
+    public Rigidbody2D plyrb;
+    public float upInte;
+    public float gravSca;
+
+    void Start()
+    {
+        plyrb = GetComponent<Rigidbody2D>();
+        plyrb.gravityScale = gravSca;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Stationary)
+            {
+                plyrb.AddForce(new Vector2(0, upInte));
+            }
+        }
+    }
+
+        
+}
