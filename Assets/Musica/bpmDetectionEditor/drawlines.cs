bf81c3c73821af0b5bda3e0e4b5efe6a335182f2
amauryxd <cdmx2472@amerike.edu.mx>
2024-09-05 10:57:41 -0600

    ya arregle lo del bpm xd

---
+++ Assets/Musica/bpmDetectionEditor/drawlines.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class drawlines : MonoBehaviour
+{
+    public int numberOfLines; // Número de líneas a dibujar
+    public float distanceBetweenLines; // Distancia entre cada línea
+    public Vector3 startPoint = Vector3.zero; // Punto de inicio de la primera línea
+    public Vector3 direction = Vector3.right; // Dirección en la que se dibujarán las líneas
+    public float lineLength; // Longitud de las líneas
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            Vector3 offset = direction * distanceBetweenLines * i;
+            Vector3 lineStart = startPoint + offset;
+            Vector3 lineEnd = lineStart + Vector3.up * lineLength; // Ajusta la longitud de la línea aquí
+
+            Gizmos.DrawLine(lineStart, lineEnd);
+        }
+    }
+}
