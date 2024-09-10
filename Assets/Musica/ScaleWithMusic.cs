d5d2e52af818b7fd488977e480d790dbe75e70ad
amauryxd <cdmx2472@amerike.edu.mx>
2024-09-10 11:17:23 -0600

    yasirve

--- Assets/Musica/ScaleWithMusic.cs
+++ Assets/Musica/ScaleWithMusic.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ScaleWithMusic : MonoBehaviour
@@ -10,24 +11,36 @@ public class ScaleWithMusic : MonoBehaviour
     public bool x;
     public bool y;
     public bool z;
-    // Start is called before the first frame update
+    
+    public bool inicio=false;
     void Start()
     {
-    
+        inicio = false;
+        StartCoroutine(offSet());
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
+        if(inicio){
         //material.color = new Color(material.color.r,material.color.g,material.color.b,(AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity);
-        if(x && AudioDetect.audioBandBuffer[band] >= 0){
-            gameObject.transform.localScale = new Vector3((AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity,gameObject.transform.localScale.y,gameObject.transform.localScale.z);
+            if(x && AudioDetect.audioBandBuffer[band] >= 0){
+                gameObject.transform.localScale = new Vector3((AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity,gameObject.transform.localScale.y,gameObject.transform.localScale.z);
+                
+            }
+            if(y && AudioDetect.audioBandBuffer[band] >=0){
+                gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x,(AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity,gameObject.transform.localScale.z);
+                
+            }
+            if(z && AudioDetect.audioBandBuffer[band] >=0){
+                gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x,gameObject.transform.localScale.y,(AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity);
+                
+            }
         }
-        if(y && AudioDetect.audioBandBuffer[band] >=0){
-            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x,(AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity,gameObject.transform.localScale.z);
-        }
-        if(z && AudioDetect.audioBandBuffer[band] >=0){
-            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x,gameObject.transform.localScale.y,(AudioDetect.audioBandBuffer[band]*(_maxIntensity - _minIntensity))+_minIntensity);
-        }
+    }
+
+    public IEnumerator offSet(){
+        yield return new WaitForSeconds(2f);
+        inicio = true;
     }
 }
